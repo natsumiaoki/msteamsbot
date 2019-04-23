1ae73e7134972b7bfad2191998c35f9a72d48206
KawanishiMasaki <gooutnow@gmail.com>
2019-04-23 14:20:10 +0900

    D to d

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const adapter = new BotFrameworkAdapter({
     //appPassword: endpointConfig.appPassword || process.env.microsoftAppPassword
 
     //make sure it is in your applications settings
-    appID: process.env.MirosoftAppID,
+    appID: process.env.MirosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
 
 });
